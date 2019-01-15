b2af8149f2b8bed3317bc73e24e4bb32e4f0c546
crcrewso <crcrewso@users.noreply.github.com>
2019-01-14 21:53:49 -0600

    Added Unit testing framework

---
+++ DicomStrictCompare/DSCTests/CudaMathematicsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DicomStrictCompare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alea;
+using Alea.CSharp;
+using Alea.Parallel;
+
+namespace DicomStrictCompare.Tests
+{
+    [TestClass()]
+    public class CudaMathematicsTests
+    {
+        [TestMethod()]
+        public void CompareTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void KernelTest()
+        {
+            double[] source = new double[] { 1.0, 1.0, 1.0, 1.0 };
+            double[] target = new double[] { 1.0, 1.0, 1.0, 1.0 };
+            double[] result = new double[4];
+            double tolerance = 0.01;
+            double epsilon = 0.001;
+            var gpu = Gpu.Default;
+            var lp = new LaunchParam(16, 256);
+
+            gpu.Launch(CudaMathematics.Kernel,lp,result, source, target, tolerance, epsilon);
+            Assert.AreEqual(0, result[0]);
+        }
+    }
+}
---
+++ DicomStrictCompare/DSCTests/X86MathematicsTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DicomStrictCompare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomStrictCompare.Tests
+{
+    [TestClass()]
+    public class X86MathematicsTests: X86Mathematics
+    {
+        [TestMethod()]
+        public void CanInitialize()
+        {
+            var testing = new X86Mathematics();
+        }
+
+
+
+        [TestMethod()]
+        public void CompareTestSameArray()
+        {
+            List<double> source = new List<double>();
+            List<double> target = new List<double>();
+            double tolerance = 0.01;
+            double epsilon = 0.001;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                source.Add(i);
+                target.Add(i);
+            }
+
+            var testing = new X86Mathematics();
+            var retCompare = testing.Compare(ref source, ref target, tolerance, epsilon);
+            Assert.AreEqual(0, retCompare);
+
+        }
+
+        [TestMethod()]
+        public void CompareTestSingleFailure()
+        {
+            List<double> source = new List<double>();
+            List<double> target = new List<double>();
+            double tolerance = 0.01;
+            double epsilon = 0.01;
+
+
+            for (double i = 0; i < 10; i++)
+            {
+                source.Add(1);
+                target.Add(1);
+            }
+
+            target[0] = 1+ tolerance+ epsilon;
+
+            var testing = new X86Mathematics();
+            var retCompare = testing.Compare(ref source, ref target, tolerance, epsilon);
+            Assert.AreEqual(1, retCompare);
+
+        }
+
+        [TestMethod()]
+        public void CompareTestSingleNotFailure()
+        {
+            List<double> source = new List<double>();
+            List<double> target = new List<double>();
+            double tolerance = 0.01;
+            double epsilon = 0.01;
+
+
+            for (double i = 0; i < 10; i++)
+            {
+                source.Add(1);
+                target.Add(1);
+            }
+
+            target[0] = 1 + tolerance - epsilon;
+
+            var testing = new X86Mathematics();
+            var retCompare = testing.Compare(ref source, ref target, tolerance, epsilon);
+            Assert.AreEqual(0, retCompare);
+
+        }
+
+
+        [TestMethod()]
+        public void CompareTestDifferencesTooSmall()
+        {
+            List<double> source = new List<double>();
+            List<double> target = new List<double>();
+            double tolerance = 0.01;
+            double epsilon = 0.001;
+
+
+            for (double i = 0; i < 10; i++)
+            {
+                source.Add(1);
+                target.Add( 1+epsilon);
+            }
+
+            var testing = new X86Mathematics();
+            var retCompare = testing.Compare(ref source, ref target, tolerance, epsilon);
+            Assert.AreEqual(0, retCompare);
+
+        }
+
+        [TestMethod()]
+        public void CompareSingleValueSame()
+        {
+            Assert.AreEqual(true, IsWithinTolerance(1.0, 1.0, 0.1));
+        }
+
+        [TestMethod()]
+        public void CompareSingleValueWithinTolerance()
+        {
+            Assert.AreEqual(true, IsWithinTolerance(1.0, 1.09, 0.1));
+        }
+
+        [TestMethod()]
+        public void CompareSingleValueAtTolerance()
+        {
+            Assert.AreEqual(false, IsWithinTolerance(1.0, 1.1, 0.1));
+        }
+
+    }
+}
