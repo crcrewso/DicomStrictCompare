40c05aff16aeae11aa4c1ab791525b222ecfb55c
crcrewso <crcrewso@users.noreply.github.com>
2020-01-19 11:27:49 -0600

    Some reorganization of the reference test files

---
+++ DicomStrictCompare/DSCTests/Model/DoseMatrixOptimalTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DicomStrictCompare.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomStrictCompare.Model.Tests
+{
+    [TestClass()]
+    public class DoseMatrixOptimalTests
+    {
+        [TestMethod()]
+        public void DoseMatrixOptimalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsInBoundsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPointDoseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPointDoseTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LatticeXYZToIndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompareDimensionsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- DicomStrictCompare/DSCTests/Model/X86MathematicsTests.cs
+++ DicomStrictCompare/DSCTests/Model/X86MathematicsTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DicomStrictCompare.Tests
+namespace DicomStrictCompare.Model.Tests
 {
     [TestClass()]
     public class X86MathematicsTests: X86Mathematics
