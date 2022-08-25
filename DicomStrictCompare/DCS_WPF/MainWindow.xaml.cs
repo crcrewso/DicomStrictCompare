88da5a49833edd6d6e74c56ce1fe4a38a75a7481
crcrewso <6089951+crcrewso@users.noreply.github.com>
2022-08-25 00:13:04 -0600

    some work on interface with working button and responsive text boxes

--- DicomStrictCompare/DCS_WPF/MainWindow.xaml.cs
+++ DicomStrictCompare/DCS_WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.Text;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,142 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
 
 namespace DCS_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region Private variables
+
+        string _sourceLabelString;
+        string _targetLabelString;
+        string _sourceDirectoryPathString;
+        string _targetDirectoryPathString;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Human readable description of the source or reference dataset 
+        /// To be used in both the application and the produced graphic outputs
+        /// </summary>
+        public string SourceLabelString
+        {
+            get { return _sourceLabelString; }
+            set
+            {
+                _sourceLabelString = value;
+                OnPropertyChanged(nameof(SourceLabelString));
+            }
+        }
+
+        /// <summary>
+        /// Human readable description of the target or test dataset 
+        /// To be used in both the application and the produced graphic outputs
+        /// </summary>
+        public string TargetLabelString
+        {
+            get { return _targetLabelString; }
+            set
+            {
+
+                _targetLabelString = value ?? _targetLabelString;
+                OnPropertyChanged(nameof(TargetLabelString));
+            }
+        }
+
+        // weird way to make sure it's never null 
+        public string SourceDirectoryPathString
+        {
+            get => _sourceDirectoryPathString;
+            set
+            {
+                if (value != null)
+                    _sourceDirectoryPathString = value;
+                OnPropertyChanged(nameof(SourceDirectoryPathString));
+
+            }
+        }
+        public string TargetDirectoryPathString
+        {
+            get => _targetDirectoryPathString;
+            set
+            {
+                if (value != null)
+                    _targetDirectoryPathString = value;
+                OnPropertyChanged(nameof(TargetDirectoryPathString));
+
+            }
+        }
+
+
+
+
+
+        #endregion
+
         public MainWindow()
         {
+
             InitializeComponent();
+            SourceLabelString = Properties.Resources.DefaultSourceLabel;
+            TargetLabelString = Properties.Resources.DefaultTargetLabel;
+            SourceDirectoryPathString = Properties.Resources.DefaultSourceDirectory;
+            TargetDirectoryPathString = Properties.Resources.DefaultTargetDirectory;
+            MainGrid.DataContext = this; // This is what binds this window's properties to the xaml view!
         }
+
+        #region field binding instructions
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        #endregion
+
+
+        #region Buttons
+        private void SourceDirectoryPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = true;
+            dialog.InitialDirectory = System.IO.Path.GetFullPath(System.IO.Directory.GetParent(path: SourceDirectoryPathString).FullName.ToString());
+            DialogResult dialogResult = dialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                SourceDirectoryPathLabel.Content = dialog.SelectedPath;
+                SourceDirectoryPathString = System.IO.Path.GetFullPath(dialog.SelectedPath);
+
+            }
+
+            OnPropertyChanged(nameof(TargetDirectoryPathString));
+        }
+
+        private void TargetDirectoryPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = true;
+            dialog.InitialDirectory = System.IO.Path.GetFullPath(System.IO.Directory.GetParent(path: TargetDirectoryPathString).FullName.ToString());
+            DialogResult dialogResult = dialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                TargetDirectoryPathLabel.Content = dialog.SelectedPath;
+                TargetDirectoryPathString = System.IO.Path.GetFullPath(dialog.SelectedPath);
+
+            }
+
+            OnPropertyChanged(nameof(TargetDirectoryPathString));
+        }
+
+        #endregion
     }
 }
