261e942fd645b103e4cd0d3a09cb70bffd445220
crcrewso <6089951+crcrewso@users.noreply.github.com>
2023-03-16 09:51:30 -0600

    Added Skeleton for new WPF program to analyze profiles

---
+++ DicomStrictCompare/DSClibrary/ParsePTW.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSClibrary
+{
+    /// <summary>
+    /// This class is able to take a PTW profile file and parse the individual profiles, including profile type, beam settings, and individual profiles. 
+    /// </summary>
+    internal class ParsePTW
+    {
+        string filename;
+
+         
+
+    }
+
+
+    internal class PTWScan
+    {
+        internal string Task_Name { get; }
+        internal string LINAC { get; }
+        internal string Modality { get; }
+        internal string Energy { get; }
+        internal string Field_Inplane { get; }
+        internal string Field_Crossplane { get; }
+
+
+
+
+    }
+}
