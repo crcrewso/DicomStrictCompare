b43dd2fcb25d1767ad0c46f48ab10e3f5d10a7c9
crcrewso <6089951+crcrewso@users.noreply.github.com>
2023-03-17 02:43:03 -0600

    added class for eventual header concept. should header be description or another name for match identifier

---
+++ DicomStrictCompare/DSClibrary/Headers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSClibrary
+{
+    public class Header
+    {
+    }
+
+    public class ProfileHeader : Header
+    {
+        public double depth { get; init; }
+
+    }
+
+    public class DoseVolumeHeader : Header
+    {
+        
+    }
+
+
+}
