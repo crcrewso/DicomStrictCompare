9567f6c99be7eee7625ef3cec4723c3e945a78bb
Cody Crewson <6089951+crcrewso@users.noreply.github.com>
2024-02-19 14:01:40 -0600

    Refactor: Changed format of settings and added a settings file, right now this is write only

--- DicomStrictCompare/DSCcore/Controller/settings.cs
+++ DicomStrictCompare/DSCcore/Controller/settings.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
 
 namespace DCSCore.Controller
 {
     /// <summary>
     /// Consolidated settings collection to undo feature creep spaghettification.  
     /// </summary>
-    public class Settings
+    public class Settings(
+        Model.Dta[] dtas
+            , bool runDoseComparisons
+            , bool runPDDComparisons
+            , bool runProfileComparisons
+            , int coresIn = 4
+            )
     {
 
         /// <summary>
@@ -17,27 +26,31 @@ namespace DCSCore.Controller
         /// </summary>
 
 
-        public Model.Dta[] Dtas { get; }
-        public bool RunDoseComparisons { get;  }
-        public bool RunPDDComparisons { get; }
-        public bool RunProfileComparisons { get; }
-        public int CpuParallel { get; private set; } = 1;
+        public Model.Dta[] Dtas { get; } = dtas;
+        public bool RunDoseComparisons { get; } = runDoseComparisons;
+        public bool RunPDDComparisons { get; } = runPDDComparisons;
+        public bool RunProfileComparisons { get; } = runProfileComparisons;
+        public int CpuParallel { get; private set; } = Math.Min(coresIn, Environment.ProcessorCount);
 
-        public Settings(
-            DCSCore.Model.Dta[] dtas
-            , bool runDoseComparisons
-            , bool runPDDComparisons
-            , bool runProfileComparisons
-            , int coresIn
-            )
+        public void Save()
         {
-            Dtas = dtas;
-            RunDoseComparisons = runDoseComparisons;
-            RunPDDComparisons = runPDDComparisons;
-            RunProfileComparisons = runProfileComparisons;
-            CpuParallel = Math.Min(coresIn, Environment.ProcessorCount);
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+
+            //settings["RunDoseComparisons"].Value = RunDoseComparisons.ToString();
+            //settings["RunPDDComparisons"].Value = RunPDDComparisons.ToString();
+            //settings["RunProfileComparisons"].Value = RunProfileComparisons.ToString();
+            settings["CpuParallel"].Value = CpuParallel.ToString() ;
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+
         }
 
 
+
     }
+
+
+
 }
